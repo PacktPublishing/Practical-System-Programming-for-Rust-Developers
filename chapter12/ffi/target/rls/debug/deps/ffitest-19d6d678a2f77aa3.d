480c8e7f8238ed7ba877744a20b485fa601473be
peshwar9 <par.esh1937@gmail.com>
2020-12-09 19:56:51 +0530

    chapter 12 FD

--- chapter12/ffi/target/rls/debug/deps/ffitest-19d6d678a2f77aa3.d
+++
@@ -1,5 +0,0 @@
-/Users/prabhueshwarla/rust/author/packt/prod/chapter12/ffi/target/rls/debug/deps/ffitest-19d6d678a2f77aa3.rmeta: src/lib.rs
-
-/Users/prabhueshwarla/rust/author/packt/prod/chapter12/ffi/target/rls/debug/deps/ffitest-19d6d678a2f77aa3.d: src/lib.rs
-
-src/lib.rs:
--- chapter12/ffi/target/rls/debug/deps/ffitest-273a3790abc26d1b.d
+++
@@ -1,5 +0,0 @@
-/Users/prabhueshwarla/rust/author/packt/prod/chapter12/ffi/target/rls/debug/deps/ffitest-273a3790abc26d1b.rmeta: src/lib.rs
-
-/Users/prabhueshwarla/rust/author/packt/prod/chapter12/ffi/target/rls/debug/deps/ffitest-273a3790abc26d1b.d: src/lib.rs
-
-src/lib.rs:
